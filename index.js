67d22be10f26369b709047354d2389beb3aba16f
joeyguo <lgsincy@gmail.com>
2017-12-02 12:49:15 +0800

    support noclear

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ var html = require("./lib/html");
 var utils = require("./lib/utils");
 
 module.exports = function (data, opts, tool) {
-    var prefix = opts.prefix,
-        frameduration = opts.frameduration * 1000;
+    
 
     var names = tool.getNames();
     var _data = utils.formateData(data, ['name']);
 
     tool.writeFile("data.js", `var data = ${JSON.stringify(_data, null, '    ')}`);
-    tool.writeFile("gka.html", html(_data, names, prefix, frameduration));
+    tool.writeFile("gka.html", html(_data, names, opts));
 };
 
