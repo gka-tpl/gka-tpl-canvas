eeb6572c26cd17e0d10ab47905d59204baf7c89f
joeyguo <lgsincy@gmail.com>
2017-12-04 23:22:04 +0800

    support split

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ var html = require("./lib/html");
 var utils = require("./lib/utils");
 
 module.exports = function (data, opts, tool) {
-    
 
-    var names = tool.getNames();
+    var names = utils.getFileNames(data);
     var _data = utils.formateData(data, ['name']);
 
     tool.writeFile("data.js", `var data = ${JSON.stringify(_data, null, '    ')}`);
-    tool.writeFile("gka.html", html(_data, names, opts));
+    tool.writeFile("gka.html", html(_data, JSON.stringify(names), opts));
 };
 
