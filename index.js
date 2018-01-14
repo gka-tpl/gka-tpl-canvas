16228c6ede91756acc8f7c75c44d927ccdca30b7
joeyguo <lgsincy@gmail.com>
2018-01-14 23:58:59 +0800

    add callback

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var html = require("./lib/html");
 var utils = require("./lib/utils");
 
-module.exports = function (data, opts, tool) {
+module.exports = function (data, opts, tool, cb) {
 
     var names = utils.getFileNames(data);
     var _data = utils.formateData(data, ['name']);
 
-    tool.writeFile("data.js", `var data = ${JSON.stringify(_data, null, '    ')}`);
-    tool.writeFile("gka.html", html(_data, JSON.stringify(names), opts));
+    tool.writeFile("data.js", `var data = ${JSON.stringify(_data, null, '    ')}`, function() {
+        tool.writeFile("gka.html", html(_data, JSON.stringify(names), opts), function() {
+            cb && cb();
+        });
+    });
 };
 
